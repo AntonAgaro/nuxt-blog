02ae6e8e2cba80884a72b22ba83f7b383e98db95
Anton Agaronov <anton.agaronov@legalbet.com>
2022-05-05 21:10:36 +0400

    feat: add admin basis

---
+++ layouts/admin.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class='wrapper'>
+    <el-container :style='{height: "100%" }'>
+      <el-aside width="250px">
+        <app-aside/>
+      </el-aside>
+      <el-main>
+        <Nuxt/>
+      </el-main>
+    </el-container>
+  </div>
+</template>
+
+<script>
+import AppAside from '~/components/admin/Aside';
+export default {
+  components: {
+    AppAside
+  }
+}
+</script>
+
+<style lang='scss' scoped>
+  .wrapper {
+    width: 100%;
+    height: 100vh;
+  }
+</style>
